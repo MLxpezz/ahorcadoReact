906df5e89d62ef89f0ac2e8eb7e5da61564bf854
MLxpezz <mlopez_098@hotmail.com>
2023-11-23 16:49:05 -0600

    img

--- src/components/subcomponents/Body.jsx
+++ src/components/subcomponents/Body.jsx
@@ -24,7 +24,7 @@ const Body = () => {
   }, [correctChar]);
 
   return <>
-    <Corpse src={`../../assets/img/${correctChar}.png`}/>
+    <Corpse src={`../../../public/${correctChar}.png`}/>
   </>;
 };
 
